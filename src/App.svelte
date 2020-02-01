690c43210dc0ff88acf0c0c38083d5eb28881eeb
Adrián López Martínez <adrian.lopez@ezzing.com>
2020-02-01 11:19:13 +0100

    chore(users-table): display table of users

--- src/App.svelte
+++ src/App.svelte
@@ -1,12 +1,12 @@
 <script>
-
+    let users = [];
     function init() {
-        fetch('https://hr.oat.taocloud.org/v1/users?limit=2000&offset=0')
+        fetch('https://hr.oat.taocloud.org/v1/users?limit=20&offset=0')
             .then((response) => {
                 return response.json();
             })
-            .then((myJson) => {
-                console.log(myJson);
+            .then((listedUsers) => {
+               users = [...listedUsers]
             });
     }
     init();
@@ -23,6 +23,22 @@
 
 <main>
 	<button on:click={getUser}> test me!</button>
+    <table>
+        <thead>
+            <th>-</th>
+            <th>First name</th>
+            <th>Last name</th>
+        </thead>
+        <tbody>
+            {#each users as user}
+            <tr>
+                <td>{user.userId + 1}</td>
+                <td>{user.firstName}</td>
+                <td>{user.lastName}</td>
+            </tr>
+            {/each}
+        </tbody>
+    </table>
 </main>
 
 <style>
@@ -39,6 +55,11 @@
 		font-size: 4em;
 		font-weight: 100;
 	}
+
+    table, tr, th {
+        border: 1px solid black;
+        border-collapse: collapse;
+    }
 
 	@media (min-width: 640px) {
 		main {
