a43d11dbe5f583a4f6d3fb10f806c19ed4e24aa7
Adrián López Martínez <adrian.lopez@ezzing.com>
2020-02-01 13:53:10 +0100

    chore(users-table): implemented dynamic pagination

--- src/App.svelte
+++ src/App.svelte
@@ -6,49 +6,56 @@
     let users = [];
 
     /**
-     * inits the component. First asks for the total number of entities to display
-     * (endpoint needs to be implemented on API) and calculates the
-     * number of pages. Then it loads the initial list of user
-     * considering that client is on page 0.
+     * inits the component loading the initial list of users
      */
     function init() {
-        const totalNumberOfItems = getTotalNumberOfItems();
-        numberOfPages = Math.ceil(totalNumberOfItems / itemsPerPage);
-        loadListOfUsers();
+        updateListOfUsers();
     }
 
     /**
-     *  Gets the total number of entities so we can calculate the number of
-     *  table pages.
-     * TODO - Replace mocked answer once API implements endpoint. We should implement this
-     * endpoint because on large table of data we can't ask for every item and calculate the
-     * quantity of them on frontend.
-     * @returns - total number of entities to request for
-    */
-    function getTotalNumberOfItems() {
-        return 100;
-    }
-
-    /**
-     *  Gets a list of users considering the current page and the number
-     * of users to display per page
-     * @params currentPage: number - current user page, if it is not provided a default value of 0 (first page) is assigned
+     *  Gets a list of users considering the current page, the items per page, and the string to find for.
+     * FIrst asks API how many users will be displayed to calculate pagination, and once the
+     * number of pages has been calculated it asks for the current page users
+     * @params currentPage: number - current user page; if it is not provided, a default value of 0 (first page) is assigned
      */
-    function loadListOfUsers(currentPage = 0) {
-        const offset = currentPage * itemsPerPage;
-        let url =`https://hr.oat.taocloud.org/v1/users?limit=${itemsPerPage}&offset=${offset}`;
-        if (stringToFind !== '') {
-            url += `&name=${stringToFind}`;
-        }
-        console.log(url);
-        fetch(url)
-            .then((response) => {
-                return response.json();
+    function updateListOfUsers(currentPage = 0) {
+        const totalNumberOfItems = getTotalNumberOfUsersToList()
+            .then((totalNumberOfItems) => {
+                numberOfPages = Math.ceil(totalNumberOfItems / itemsPerPage);
+                const offset = currentPage * itemsPerPage;
+                let url =`https://hr.oat.taocloud.org/v1/users?limit=${itemsPerPage}&offset=${offset}&name=${stringToFind}`;
+                getUsers(url)
+                    .then((listedUsers) => users = [...listedUsers]);
+
             })
-            .then((listedUsers) => {
-               users = [...listedUsers];
-               console.log(users);
-            });
+    }
+
+    /**
+     *  Gets the total number of users that satisfy the filters.
+     * FIXME - Currently the number of users to list is calcualted using the API endpoint to list the users (/users),  but with a mocked limit of 1000.
+     *The endpoint must be replaced  once API implements a new endpoint to just get the number of coincidences. We should
+     * implement this endpoint because on large table of data we can't ask for all coincidences and calculate
+     * the quantity of them on frontend. Actually at this point is redundant to ask twice
+     * for the list of users, but I leave it ready to just change the endpoint
+     * once API implements its before next release.
+     * @returns: promise - a promise with the total number after applying filters
+    */
+    function getTotalNumberOfUsersToList() {
+        let url =`https://hr.oat.taocloud.org/v1/users?limit=1000&offset=0&name=${stringToFind}`;
+        // TODO - This endpoint must be replaced with the new one!
+        return getUsers(url)
+            .then((listedUsers) => listedUsers.length);
+    }
+
+    /**
+     * Request API a list of users
+     * @returns : promise - promise containing the list of users
+     */
+    function getUsers(url) {
+               return  fetch(url)
+                    .then((response) => {
+                        return response.json();
+                    });
     }
 
     /**
@@ -73,7 +80,7 @@
     <table>
         <thead>
 
-            <th><input type='text' bind:value={stringToFind} on:input={() => {loadListOfUsers()}}></th>
+            <th><input type='text' bind:value={stringToFind} on:input={() => {updateListOfUsers()}}></th>
             <th>First name</th>
             <th>Last name</th>
         </thead>
@@ -88,7 +95,7 @@
         </tbody>
         <tfoot>
             {#each Array(numberOfPages) as _, pageIndex}
-                <button on:click={() => {loadListOfUsers(pageIndex)}}>{pageIndex + 1}</button>
+                <button on:click={() => {updateListOfUsers(pageIndex)}}>{pageIndex + 1}</button>
             {/each}
         </tfoot>
     </table>
