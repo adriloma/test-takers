e0f6899e05ce3fc0d1068063b82c3b5ad767f090
Adrián López Martínez <adrian.lopez@ezzing.com>
2020-02-01 11:59:49 +0100

    chore(users-table): implemented pagination

--- src/App.svelte
+++ src/App.svelte
@@ -1,15 +1,51 @@
 <script>
+
+    let numberOfPages = 1;
+    let itemsPerPage = 10;
     let users = [];
+
+    /**
+     * inits the component. First asks for the total number of entities to display
+     * (endpoint needs to be implemented on API) and calculates the
+     * number of pages. Then it loads the initial list of user
+     * considering that client is on page 0.
+     */
     function init() {
-        fetch('https://hr.oat.taocloud.org/v1/users?limit=20&offset=0')
+        const totalNumberOfItems = getTotalNumberOfItems();
+        numberOfPages = Math.ceil(totalNumberOfItems / itemsPerPage);
+        loadListOfUsers();
+    }
+
+    /**
+     *  Gets the total number of entities so we can calculate the number of
+     *  table pages.
+     * TODO - Replace mocked answer once API implements endpoint.
+     * @returns - total number of entities to request for
+    */
+    function getTotalNumberOfItems() {
+        return 100;
+    }
+
+    /**
+     *  Gets a list of users considering the current page and the number
+     * of users to display per page
+     * @params currentPage: number - current user page, if it is not provided a default value of 0 (first page) is assigned
+     */
+    function loadListOfUsers(currentPage = 0) {
+        const offset = currentPage * itemsPerPage;
+        fetch(`https://hr.oat.taocloud.org/v1/users?limit=${itemsPerPage}&offset=${offset}`)
             .then((response) => {
                 return response.json();
             })
             .then((listedUsers) => {
-               users = [...listedUsers]
+               users = [...listedUsers];
             });
     }
-    init();
+
+    /**
+     * Gets a user information
+     * TODO - Removed mocked user id
+    */
     function getUser() {
         fetch('https://hr.oat.taocloud.org/v1/user/0')
             .then((response) => {
@@ -19,6 +55,8 @@
                 console.log(myJson);
             });
     }
+
+ init();
 </script>
 
 <main>
@@ -30,7 +68,7 @@
             <th>Last name</th>
         </thead>
         <tbody>
-            {#each users as user}
+            {#each users as user (user.userId)}
             <tr>
                 <td>{user.userId + 1}</td>
                 <td>{user.firstName}</td>
@@ -38,6 +76,11 @@
             </tr>
             {/each}
         </tbody>
+        <tfoot>
+            {#each Array(numberOfPages) as _, pageIndex}
+                <button on:click={() => {loadListOfUsers(pageIndex)}}>{pageIndex + 1}</button>
+            {/each}
+        </tfoot>
     </table>
 </main>
 
@@ -49,16 +92,13 @@
 		margin: 0 auto;
 	}
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
     table, tr, th {
         border: 1px solid black;
         border-collapse: collapse;
+    }
+
+    div.table-pagination {
+
     }
 
 	@media (min-width: 640px) {
